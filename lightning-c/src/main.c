c8a46d80f6315c668049ba0e7057770eddd58e23
JonathanGzzBen <jonathangzzben@gmail.com>
2022-03-04 20:03:11 -0600

    Make lightning_c use UTF-8 on Windows

--- lightning-c/src/main.c
+++ lightning-c/src/main.c
@@ -39,11 +39,14 @@ CuentaUsuario leer_cuenta() {
 }
 
 void print_cuenta(const CuentaUsuario* cuenta) {
-  printf("Nombre: %-25sUsuario: %-25sPassword: %-25sEdad: %-3d\n",
+  printf("Nombre: %-25sUsuario: %-25sContraseña: %-25sEdad: %-3d\n",
          cuenta->nombre, cuenta->usuario, cuenta->password, cuenta->edad);
 }
 
 int main() {
+#ifdef _WIN32
+  system("chcp 65001 > nul");
+#endif
   CuentaUsuario cuenta = leer_cuenta();
   print_cuenta(&cuenta);
   return 0;
