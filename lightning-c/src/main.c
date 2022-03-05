368f86b916a68313ec521ddab409cc00d0788d7b
JonathanGzzBen <jonathangzzben@gmail.com>
2022-03-04 19:06:17 -0600

    Add input functions without validations

--- lightning-c/src/main.c
+++ lightning-c/src/main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
-static const size_t LONGITUD_CUENTA_NOMBRE = 20;
-static const size_t LONGITUD_CUENTA_USUARIO = 20;
-static const size_t LONGITUD_CUENTA_PASSWORD = 20;
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct {
   char* nombre;
@@ -11,16 +9,42 @@ typedef struct {
   int edad;
 } CuentaUsuario;
 
+static const size_t LONGITUD_CUENTA_NOMBRE = 20;
+static const size_t LONGITUD_CUENTA_USUARIO = 20;
+static const size_t LONGITUD_CUENTA_PASSWORD = 20;
+
+static const CuentaUsuario cuentas[];
+
+char* leer_cadena(const size_t len);
+CuentaUsuario leer_cuenta();
+void print_cuenta(const CuentaUsuario* cuenta);
+
+char* leer_cadena(const size_t len) {
+  char* cadena;
+  cadena = (char*)malloc(len);
+  fgets(cadena, len, stdin);
+  cadena[strcspn(cadena, "\n")] = 0;
+  return cadena;
+}
+
+CuentaUsuario leer_cuenta() {
+  CuentaUsuario cuenta = {.edad = 18};
+  printf("Ingrese su nombre: ");
+  cuenta.nombre = leer_cadena(LONGITUD_CUENTA_NOMBRE);
+  printf("Ingrese un usuario: ");
+  cuenta.usuario = leer_cadena(LONGITUD_CUENTA_USUARIO);
+  printf("Ingrese una contraseña: ");
+  cuenta.password = leer_cadena(LONGITUD_CUENTA_PASSWORD);
+  return cuenta;
+}
+
 void print_cuenta(const CuentaUsuario* cuenta) {
-  printf("Nombre: %s\tUsuario: %s\tEdad: %d\n", cuenta->nombre, cuenta->usuario,
-         cuenta->edad);
+  printf("Nombre: %-25sUsuario: %-25sPassword: %-25sEdad: %-3d\n",
+         cuenta->nombre, cuenta->usuario, cuenta->password, cuenta->edad);
 }
 
 int main() {
-  CuentaUsuario primeraCuenta = {.edad = 18,
-                                 .nombre = "Jonathan Gonzalez",
-                                 .usuario = "jonark",
-                                 .password = "comer"};
-  print_cuenta(&primeraCuenta);
+  CuentaUsuario cuenta = leer_cuenta();
+  print_cuenta(&cuenta);
   return 0;
 }
