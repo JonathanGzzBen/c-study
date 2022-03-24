e58bf603091ee4789f1fb6592995c0398978bf17
JonathanGzzBen <jonathangzzben@gmail.com>
2022-03-24 10:03:50 -0600

    Add Windows API code

---
+++ windowsapi/screen-size/main.c
@@ -0,0 +1,13 @@
+#include <wchar.h>
+#include <windows.h>
+
+#pragma comment(lib, "user32.lib")
+
+int wmain(void) {
+  int x = GetSystemMetrics(SM_CXSCREEN);
+  int y = GetSystemMetrics(SM_CYSCREEN);
+
+  wprintf(L"The screen size is: %dx%d\n", x, y);
+
+  return 0;
+}
