88e4e752be7c504a6e38eb307a0ba096b31f7e6c
JonathanGzzBen <jonathangzzben@gmail.com>
2022-03-25 19:48:28 -0600

    Change font size of calculator

--- gtk3-calculator/src/main.c
+++ gtk3-calculator/src/main.c
@@ -11,10 +11,24 @@ int main(int argc, char *argv[]) {
   GtkBuilder *gtkBuilder = gtk_builder_new();
   gtk_builder_add_from_file(gtkBuilder, "ui/calculator.glade", NULL);
   GtkWidget *window =
-      GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "mywindow"));
+      GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "main-window"));
   g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
+  GtkTextView *operationTextView = GTK_TEXT_VIEW(gtk_builder_get_object(gtkBuilder, "operation"));
+  GtkTextBuffer *textBuffer = gtk_text_view_get_buffer(operationTextView);
+  gtk_text_buffer_set_text(textBuffer, "000000", -1);
+
+  GtkTextView *prev_operation_text_view = GTK_TEXT_VIEW(gtk_builder_get_object(gtkBuilder, "prev-operation"));
+  GtkTextBuffer *prev_operation_text_buffer = gtk_text_view_get_buffer(prev_operation_text_view);
+  gtk_text_buffer_set_text(prev_operation_text_buffer, "000000", -1);
+
   gtk_builder_connect_signals(gtkBuilder, NULL);
+
+  GtkCssProvider *cssProvider = gtk_css_provider_new();
+  gtk_css_provider_load_from_path(cssProvider, "gtk-calculator.css", NULL);
+  GdkScreen* screen = gtk_widget_get_screen(window);
+  gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(cssProvider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+
   gtk_widget_show(window);
   gtk_main();
   return 0;
