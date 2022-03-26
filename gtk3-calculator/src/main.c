0cc5f4eeb481978ee5ceb5fc73c79215d8b30337
JonathanGzzBen <jonathangzzben@gmail.com>
2022-03-25 20:21:52 -0600

    Add signal handlers for buttons in calculator

--- gtk3-calculator/src/main.c
+++ gtk3-calculator/src/main.c
@@ -1,35 +1,39 @@
 #include <gtk/gtk.h>
-#include <stdio.h>
 
-G_MODULE_EXPORT void print_hello() {
-  puts("hello");
+GtkTextBuffer *operation_text_buffer;
+
+GtkTextBuffer *prev_operation_text_buffer;
+
+G_MODULE_EXPORT void on_pressed_button(GtkButton* self, gpointer user_data) {
+  const gchar* label = gtk_button_get_label(self);
+  gtk_text_buffer_set_text(operation_text_buffer, label, -1);
 }
 
 int main(int argc, char *argv[]) {
   gtk_init(&argc, &argv);
 
-  GtkBuilder *gtkBuilder = gtk_builder_new();
-  gtk_builder_add_from_file(gtkBuilder, "ui/calculator.glade", NULL);
-  GtkWidget *window =
-      GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "main-window"));
-  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+  GtkBuilder *builder = gtk_builder_new();
+  gtk_builder_add_from_file(builder, "ui/calculator.glade", NULL);
+  GtkWidget *main_window =
+      GTK_WIDGET(gtk_builder_get_object(builder, "main-window"));
+  g_signal_connect(main_window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-  GtkTextView *operationTextView = GTK_TEXT_VIEW(gtk_builder_get_object(gtkBuilder, "operation"));
-  GtkTextBuffer *textBuffer = gtk_text_view_get_buffer(operationTextView);
-  gtk_text_buffer_set_text(textBuffer, "000000", -1);
+  GtkTextView *operation_text_view = GTK_TEXT_VIEW(gtk_builder_get_object(builder, "operation"));
+  operation_text_buffer = gtk_text_view_get_buffer(operation_text_view);
+  gtk_text_buffer_set_text(operation_text_buffer, "000000", -1);
 
-  GtkTextView *prev_operation_text_view = GTK_TEXT_VIEW(gtk_builder_get_object(gtkBuilder, "prev-operation"));
-  GtkTextBuffer *prev_operation_text_buffer = gtk_text_view_get_buffer(prev_operation_text_view);
+  GtkTextView*  prev_operation_text_view = GTK_TEXT_VIEW(gtk_builder_get_object(builder, "prev-operation"));
+  prev_operation_text_buffer = gtk_text_view_get_buffer(prev_operation_text_view);
   gtk_text_buffer_set_text(prev_operation_text_buffer, "000000", -1);
 
-  gtk_builder_connect_signals(gtkBuilder, NULL);
+  gtk_builder_connect_signals(builder, NULL);
 
-  GtkCssProvider *cssProvider = gtk_css_provider_new();
-  gtk_css_provider_load_from_path(cssProvider, "gtk-calculator.css", NULL);
-  GdkScreen* screen = gtk_widget_get_screen(window);
-  gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(cssProvider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+  GtkCssProvider *css_provider = gtk_css_provider_new();
+  gtk_css_provider_load_from_path(css_provider, "gtk-calculator.css", NULL);
+  GdkScreen* screen = gtk_widget_get_screen(main_window);
+  gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(css_provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
 
-  gtk_widget_show(window);
+  gtk_widget_show(main_window);
   gtk_main();
   return 0;
 }
